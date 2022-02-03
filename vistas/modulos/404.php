591a5cf8e175c7de193fa9fd293ce6a39ed96f1e
Sergio Pereira Jorquera <sergiopereira.jorquera@gmail.com>
2022-02-03 17:08:29 -0300

    guardar

---
+++ vistas/modulos/404.php
@@ -0,0 +1,37 @@
+
+  <div class="content-wrapper">
+
+    <section class="content-header">
+      <h1>
+        Página no Encontrada
+        </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Página no Encontrada</li>
+      </ol>
+    </section>
+
+    <section class="content">
+      <div class="error-page">
+      
+      <h2 class="headline text-primary">404</h2>
+
+      <div class="error-content">
+        <h3><i class="fa fa-warning text-primary"></i>
+
+        Oooops! Página no encontrada
+        </h3>
+
+        <p>
+          Ingresa en el Menú Lateral y allí podrás encontrar las páginas disponibles.
+          Tambien puedes regresar haciendo <a href="inicio">click aquí.</a>
+
+        </p>
+
+      </div>
+      </div>
+    </section>
+
+   
+  </div>
+  <!-- /.content-wrapper -->
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+	
+<strong>Sergio Pereira 2022 <a href="https://github.com/sergiopereiraj/" target="blank">My Repository</a>
+	
+</strong>
+Todos los derechos reservados.
+
+
+</footer>	
